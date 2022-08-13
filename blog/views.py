4249d784d4f18963f1f5c99de51b10c6211efb56
JessMair <jessiegrewal11@gmail.com>
2022-08-13 11:15:04 +0000

    Add views and wire up urls

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Post 
 
-from .models import post 
+
 
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwards):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.coments.filter(approved=True).order_by('create_on')
+        liked = False
+        if post.ikes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
+
+
+
