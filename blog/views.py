8e084511a153a10871bcc2307e03e2a05630e309
JessMair <jessiegrewal11@gmail.com>
2022-08-22 11:22:22 +0000

    Add alert messages for users when they sign in or out of the site

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from .models import Post 
+from .models import Post
 from .forms import CommentForm
 
 
@@ -69,15 +69,12 @@ class PostDetail(View):
         )
 
 class PostLike(View):
-
-    def post(self, request, slug, *args, **kwarg):
-        post = get_object_or_404(post, slug=slug)
-
+    
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
 
-            return HttpResponseRedirect(reverse('post_detail', args=[slug]))
-
-
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
