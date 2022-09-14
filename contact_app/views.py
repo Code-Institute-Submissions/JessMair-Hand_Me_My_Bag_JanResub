fe5356d9ba0dd786cc2784dba5fe5a61f879f186
JessMair <jessiegrewal11@gmail.com>
2022-09-14 21:05:20 +0000

    Add rating to fields in forms.py

--- contact_app/views.py
+++ contact_app/views.py
@@ -10,11 +10,11 @@ def contact(request):
     if request.method == "POST":
         contact = Contact()
         name = request.POST.get('name')
-        email = request.POST.get('email')
+        subject = request.POST.get('subject')
         message = request.POST.get('message')
 
         contact.name = name
-        contact.email = email
+        contact.subject = subject
         contact.message = message
         contact.save()
         
