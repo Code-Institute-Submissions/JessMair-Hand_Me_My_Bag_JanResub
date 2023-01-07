4b996ff56dd46c19b5514d7bdfe1da6fbac583a0
JessMair <jessiegrewal11@gmail.com>
2023-01-07 13:26:23 +0000

    Alter contact form to include an email field

--- contact_app/models.py
+++ contact_app/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Contact(models.Model):
-    name = models.CharField(max_length=158)
-    subject = models.CharField(max_length=200)
-    message = models.TextField()
+    name = models.CharField(max_length=158, blank=False)
+    email = models.EmailField(blank=False, default='')
+    message = models.TextField(blank=False)
 
     def __str__(self):
         return self.name
